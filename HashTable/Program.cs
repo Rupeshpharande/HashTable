6c79104d38318130b58791dcd15c17f4ed54fe07
Rupeshpharande <pharanderupesh@gmail.com>
2024-11-14 01:12:44 +0530

    Add project files.

---
+++ HashTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDemos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("IND", 91);
+            ht.Add("USA", 1);
+            ht.Add("AUS", 63);
+
+            ht.Remove("IND");
+           
+
+            foreach (DictionaryEntry items in ht)
+            {
+                Console.WriteLine(items.Key + " "+items.Value);
+            }
+        }
+    }
+}
